848ff280b0db30cf0540306f7ff5eff64cea2501
Alireza Hashemi <ahashemi1375@gmail.com>
2024-01-30 21:35:56 -0500

    test cases

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         for i in range(n):
             input_list[0].append(i*alpha)
             input_list[1].append('s'+str(i//2+1) if i % 2 == 0 else 'e'+str(i//2+1))
-        count = count_intersection(input_list)
+        count = count_intersection(input_list, draw=False, save=False)
         assert count == 0, f'Test failed, Expected {0} but got {count}'
         
         'Test 2: all diameters intersect'
@@ -21,19 +21,19 @@ if __name__ == "__main__":
             input_list[0].append(i*alpha)
             input_list[1].append('s'+str(i+1) if i < (n//2) else 'e'+str(i+1-n//2))
     
-        count = count_intersection(input_list)
+        count = count_intersection(input_list, draw=False, save=False)
         assert count == (n//2)*(n//2-1)//2, f'Test failed, Expected {n//2*(n//2-1)//2} but got {count}'
     
     'Test 3: given example #1'
     input_list = [[0.78, 1.47, 1.77, 3.92], ['s1', 's2', 'e1', 'e2']]
     
-    count = count_intersection(input_list)
+    count = count_intersection(input_list, draw=False, save=False)
     assert count == 1, f'Test failed, Expected {1} but got {count}'
     
     'Test 4: given example #2'
     input_list = [[0.9, 1.3, 1.7, 2.92], ['s1', 'e1', 's2', 'e2']]
     
-    count = count_intersection(input_list)
+    count = count_intersection(input_list, draw=False, save=False)
     assert count == 0, f'Test failed, Expected {0} but got {count}'
     
     print('All test cases passed!')
